651aa6bff4faa6e48cc62a54c2d462a0804ab199
md-ashikulislam <62753529+md-ashikulislam@users.noreply.github.com>
2025-04-05 23:02:27 +0600

    Update

--- Seg_UKAN_SE/archs3.py
+++ Seg_UKAN_SE/archs3.py
@@ -35,13 +35,20 @@ class ChannelAttention(nn.Module):
         return self.sigmoid(out)
 
 class SpatialAttention(nn.Module):
-    def __init__(self, kernel_size=7):  # default kernel size
+    def __init__(self, kernel_size=7):
         super(SpatialAttention, self).__init__()
         assert kernel_size in (3, 7), 'kernel size must be 3 or 7'
         padding = 3 if kernel_size == 7 else 1
 
         self.conv = nn.Conv2d(2, 1, kernel_size=kernel_size, padding=padding, bias=False)
         self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x_cat = torch.cat([avg_out, max_out], dim=1)
+        attention = self.sigmoid(self.conv(x_cat))
+        return x * attention
 
 class CBAM(nn.Module):
     def __init__(self, planes, ratio=8, kernel_size=7):
