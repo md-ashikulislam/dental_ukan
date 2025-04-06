873a2e4c16a9e3b4fff617999588d368dd70e99a
md-ashikulislam <62753529+md-ashikulislam@users.noreply.github.com>
2025-04-06 21:13:29 +0600

    Update

--- Seg_UKAN/train.py
+++ Seg_UKAN/train.py
@@ -424,6 +424,7 @@ def main():
 
     # create model
     model = archs.__dict__[config['arch']](config['num_classes'], config['input_channels'], config['deep_supervision'], embed_dims=config['input_list'], no_kan=config['no_kan'])
+    print(f"no_kan mode is {'ACTIVATED' if config['no_kan'] else 'NOT activated'}")
 
 
     # Count parameters and print PrettyTable
