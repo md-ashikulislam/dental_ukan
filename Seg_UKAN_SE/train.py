a913739d47defb07bccc3df0a74502e17a8026e7
md-ashikulislam <62753529+md-ashikulislam@users.noreply.github.com>
2025-04-06 15:07:39 +0600

    Update

--- Seg_UKAN_SE/train.py
+++ Seg_UKAN_SE/train.py
@@ -383,7 +383,7 @@ def main():
     cudnn.benchmark = True
 
     # create model
-    model = archs3.__dict__[config['arch']](config['num_classes'], config['input_channels'], config['deep_supervision'], embed_dims=config['input_list'], no_kan=config['no_kan'])
+    model = archs4.__dict__[config['arch']](config['num_classes'], config['input_channels'], config['deep_supervision'], embed_dims=config['input_list'], no_kan=config['no_kan'])
 
 
     total_params = count_parameters(model)
