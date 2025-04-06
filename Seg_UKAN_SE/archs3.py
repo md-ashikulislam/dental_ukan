582b2d67c4567f740c7b64a2c2112cab8f3cea11
md-ashikulislam <62753529+md-ashikulislam@users.noreply.github.com>
2025-04-06 14:31:18 +0600

    Update

--- Seg_UKAN_SE/archs3.py
+++ Seg_UKAN_SE/archs3.py
@@ -1,13 +1,5 @@
 import torch
 from torch import nn
-import torch.nn.functional as F
-from timm.layers import DropPath, to_2tuple, trunc_normal_
-import math
-
-_all_ = ['UKAN_CBAM'] 
-
-import torch
-from torch import nn
 import torchvision
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
@@ -20,42 +12,35 @@ from timm.layers import DropPath, to_2tuple, trunc_normal_
 import math
 from kan import KANLinear, KAN
 
-_all_ = ['UKAN_CBAM'] 
+_all_ = ['UKAN_CA'] 
 
-import torch
-from torch import nn
-import torchvision
-from torch.autograd import Variable
-from torch.utils.data import DataLoader
-from torchvision import transforms
-from torchvision.utils import save_image
-import torch.nn.functional as F
-import os
-import matplotlib.pyplot as plt
-from timm.layers import DropPath, to_2tuple, trunc_normal_
-import math
-from kan import KANLinear, KAN
-
-_all_ = ['UKAN_SEA'] 
-
-
-class SEAttention(nn.Module):
-    def __init__(self, channel, reduction=16):
-        super(SEAttention, self).__init__()
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.fc = nn.Sequential(
-            nn.Linear(channel, channel // reduction, bias=False),
-            nn.ReLU(inplace=True),
-            nn.Linear(channel // reduction, channel, bias=False)
-        )
+class CoordAttention(nn.Module):
+    def __init__(self, in_channels, reduction=16):
+        super(CoordAttention, self).__init__()
+        self.pool_h = nn.AdaptiveAvgPool2d((None, 1))
+        self.pool_w = nn.AdaptiveAvgPool2d((1, None))
+        mid_channels = max(8, in_channels // reduction)
+        self.conv1 = nn.Conv2d(in_channels, mid_channels, kernel_size=1, stride=1, padding=0)
+        self.bn = nn.BatchNorm2d(mid_channels)
+        self.act = nn.ReLU(inplace=True)
+        self.conv_h = nn.Conv2d(mid_channels, in_channels, kernel_size=1, stride=1, padding=0)
+        self.conv_w = nn.Conv2d(mid_channels, in_channels, kernel_size=1, stride=1, padding=0)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        b, c, _, _ = x.size()
-        y = self.avg_pool(x).view(b, c)
-        y = self.fc(y).view(b, c, 1, 1)
-        y = self.sigmoid(y)
-        return x * y.expand_as(x)
+        b, c, h, w = x.size()
+        x_h = self.pool_h(x)
+        x_w = self.pool_w(x)
+        y = torch.cat([x_h, x_w], dim=2)
+        y = self.conv1(y)
+        y = self.bn(y)
+        y = self.act(y)
+        x_h, x_w = torch.split(y, [h, w], dim=2)
+        a_h = self.conv_h(x_h)
+        a_w = self.conv_w(x_w)
+        a_h = self.sigmoid(a_h)
+        a_w = self.sigmoid(a_w)
+        return x * a_h * a_w
 
 class KANLayer(nn.Module):
     def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0., no_kan=False):
@@ -115,10 +100,10 @@ class KANLayer(nn.Module):
             self.fc2 = nn.Linear(hidden_features, out_features)
             self.fc3 = nn.Linear(hidden_features, out_features)
 
-        # Add SE Attention after each DW convolution
-        self.se1 = SEAttention(hidden_features)
-        self.se2 = SEAttention(hidden_features)
-        self.se3 = SEAttention(hidden_features)
+        # Add Coordinate Attention after each DW convolution
+        self.ca1 = CoordAttention(hidden_features)
+        self.ca2 = CoordAttention(hidden_features)
+        self.ca3 = CoordAttention(hidden_features)
 
         self.dwconv_1 = DW_bn_relu(hidden_features)
         self.dwconv_2 = DW_bn_relu(hidden_features)
@@ -149,21 +134,21 @@ class KANLayer(nn.Module):
         x = x.reshape(B, N, C).contiguous()
         x = self.dwconv_1(x, H, W)
         x = x.transpose(1, 2).view(B, C, H, W)
-        x = self.se1(x)
+        x = self.ca1(x)
         x = x.flatten(2).transpose(1, 2)
 
         x = self.fc2(x.reshape(B*N, C))
         x = x.reshape(B, N, C).contiguous()
         x = self.dwconv_2(x, H, W)
         x = x.transpose(1, 2).view(B, C, H, W)
-        x = self.se2(x)
+        x = self.ca2(x)
         x = x.flatten(2).transpose(1, 2)
 
         x = self.fc3(x.reshape(B*N, C))
         x = x.reshape(B, N, C).contiguous()
         x = self.dwconv_3(x, H, W)
         x = x.transpose(1, 2).view(B, C, H, W)
-        x = self.se3(x)
+        x = self.ca3(x)
         x = x.flatten(2).transpose(1, 2)
     
         return x
@@ -179,11 +164,11 @@ class ConvLayer(nn.Module):
             nn.BatchNorm2d(out_ch),
             nn.ReLU(inplace=True)
         )
-        self.se = SEAttention(out_ch)  # Add SE attention
+        self.ca = CoordAttention(out_ch)  # Add Coordinate Attention
 
     def forward(self, input):
         x = self.conv(input)
-        x = self.se(x)
+        x = self.ca(x)
         return x
 
 class D_ConvLayer(nn.Module):
@@ -197,35 +182,21 @@ class D_ConvLayer(nn.Module):
             nn.BatchNorm2d(out_ch),
             nn.ReLU(inplace=True)
         )
-        self.se = SEAttention(out_ch)  # Add SE attention
+        self.ca = CoordAttention(out_ch)  # Add Coordinate Attention
 
     def forward(self, input):
         x = self.conv(input)
-        x = self.se(x)
-        return x
-class DW_bn_relu(nn.Module):
-    def __init__(self, dim=768):
-        super(DW_bn_relu, self).__init__()
-        self.dwconv = nn.Conv2d(dim, dim, 3, 1, 1, bias=True, groups=dim)
-        self.bn = nn.BatchNorm2d(dim)
-        self.relu = nn.ReLU()
-
-    def forward(self, x, H, W):
-        B, N, C = x.shape
-        x = x.transpose(1, 2).view(B, C, H, W)
-        x = self.dwconv(x)
-        x = self.bn(x)
-        x = self.relu(x)
-        x = x.flatten(2).transpose(1, 2)
+        x = self.ca(x)
         return x
 
+# The rest of the code remains unchanged
 class KANBlock(nn.Module):
-    def __init__(self, dim, drop=0., drop_path=0., norm_layer=nn.LayerNorm):
+    def __init__(self, dim, drop=0., drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm, no_kan=False):
         super().__init__()
         self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
         self.norm2 = norm_layer(dim)
         mlp_hidden_dim = int(dim)
-        self.layer = KANLayer(in_features=dim, hidden_features=mlp_hidden_dim, drop=drop)
+        self.layer = KANLayer(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop, no_kan=no_kan)
         self.apply(self._init_weights)
 
     def _init_weights(self, m):
@@ -247,6 +218,34 @@ class KANBlock(nn.Module):
         x = x + self.drop_path(self.layer(self.norm2(x), H, W))
         return x
 
+class DWConv(nn.Module):
+    def __init__(self, dim=768):
+        super(DWConv, self).__init__()
+        self.dwconv = nn.Conv2d(dim, dim, 3, 1, 1, bias=True, groups=dim)
+
+    def forward(self, x, H, W):
+        B, N, C = x.shape
+        x = x.transpose(1, 2).view(B, C, H, W)
+        x = self.dwconv(x)
+        x = x.flatten(2).transpose(1, 2)
+        return x
+
+class DW_bn_relu(nn.Module):
+    def __init__(self, dim=768):
+        super(DW_bn_relu, self).__init__()
+        self.dwconv = nn.Conv2d(dim, dim, 3, 1, 1, bias=True, groups=dim)
+        self.bn = nn.BatchNorm2d(dim)
+        self.relu = nn.ReLU()
+
+    def forward(self, x, H, W):
+        B, N, C = x.shape
+        x = x.transpose(1, 2).view(B, C, H, W)
+        x = self.dwconv(x)
+        x = self.bn(x)
+        x = self.relu(x)
+        x = x.flatten(2).transpose(1, 2)
+        return x
+
 class PatchEmbed(nn.Module):
     def __init__(self, img_size=224, patch_size=7, stride=4, in_chans=3, embed_dim=768):
         super().__init__()
@@ -257,7 +256,7 @@ class PatchEmbed(nn.Module):
         self.H, self.W = img_size[0] // patch_size[0], img_size[1] // patch_size[1]
         self.num_patches = self.H * self.W
         self.proj = nn.Conv2d(in_chans, embed_dim, kernel_size=patch_size, stride=stride,
-                            padding=(patch_size[0] // 2, patch_size[1] // 2))
+                              padding=(patch_size[0] // 2, patch_size[1] // 2))
         self.norm = nn.LayerNorm(embed_dim)
         self.apply(self._init_weights)
 
@@ -283,101 +282,107 @@ class PatchEmbed(nn.Module):
         x = self.norm(x)
         return x, H, W
 
-class UKAN_SEA(nn.Module):
+class UKAN_CA(nn.Module):
     def __init__(self, num_classes, input_channels=3, deep_supervision=False, img_size=224, patch_size=16, in_chans=3, 
                  embed_dims=[256, 320, 512], no_kan=False, drop_rate=0., drop_path_rate=0., norm_layer=nn.LayerNorm, 
                  depths=[1, 1, 1], **kwargs):
         super().__init__()
-       
-        self.encoder1 = ConvLayer(input_channels, embed_dims[0]//8)
-        self.encoder2 = ConvLayer(embed_dims[0]//8, embed_dims[0]//4)
-        self.encoder3 = ConvLayer(embed_dims[0]//4, embed_dims[0])
 
-        self.norm3 = nn.LayerNorm(embed_dims[1])
-        self.norm4 = nn.LayerNorm(embed_dims[2])
-        self.dnorm3 = nn.LayerNorm(embed_dims[1])
-        self.dnorm4 = nn.LayerNorm(embed_dims[0])
+        kan_input_dim = embed_dims[0]
 
-        self.block1 = nn.ModuleList([KANBlock(dim=embed_dims[1])])
-        self.block2 = nn.ModuleList([KANBlock(dim=embed_dims[2])])
-        self.dblock1 = nn.ModuleList([KANBlock(dim=embed_dims[1])])
-        self.dblock2 = nn.ModuleList([KANBlock(dim=embed_dims[0])])
+        self.encoder1 = ConvLayer(input_channels, kan_input_dim//8)  
+        self.encoder2 = ConvLayer(kan_input_dim//8, kan_input_dim//4)  
+        self.encoder3 = ConvLayer(kan_input_dim//4, kan_input_dim)
 
-        self.patch_embed3 = PatchEmbed(
-            img_size=img_size // 4, 
-            patch_size=3, 
-            stride=2, 
-            in_chans=embed_dims[0], 
-            embed_dim=embed_dims[1]
-        )
-        self.patch_embed4 = PatchEmbed(
-            img_size=img_size // 8, 
-            patch_size=3, 
-            stride=2, 
-            in_chans=embed_dims[1], 
-            embed_dim=embed_dims[2]
-        )
+        self.norm3 = norm_layer(embed_dims[1])
+        self.norm4 = norm_layer(embed_dims[2])
+        self.dnorm3 = norm_layer(embed_dims[1])
+        self.dnorm4 = norm_layer(embed_dims[0])
 
-        self.decoder1 = D_ConvLayer(embed_dims[2], embed_dims[1])
-        self.decoder2 = D_ConvLayer(embed_dims[1], embed_dims[0])
-        self.decoder3 = D_ConvLayer(embed_dims[0], embed_dims[0]//4)
+        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, sum(depths))]
+
+        self.block1 = nn.ModuleList([KANBlock(
+            dim=embed_dims[1], 
+            drop=drop_rate, drop_path=dpr[0], norm_layer=norm_layer
+        )])
+
+        self.block2 = nn.ModuleList([KANBlock(
+            dim=embed_dims[2],
+            drop=drop_rate, drop_path=dpr[1], norm_layer=norm_layer
+        )])
+
+        self.dblock1 = nn.ModuleList([KANBlock(
+            dim=embed_dims[1], 
+            drop=drop_rate, drop_path=dpr[0], norm_layer=norm_layer
+        )])
+
+        self.dblock2 = nn.ModuleList([KANBlock(
+            dim=embed_dims[0], 
+            drop=drop_rate, drop_path=dpr[1], norm_layer=norm_layer
+        )])
+
+        self.patch_embed3 = PatchEmbed(img_size=img_size // 4, patch_size=3, stride=2, in_chans=embed_dims[0], embed_dim=embed_dims[1])
+        self.patch_embed4 = PatchEmbed(img_size=img_size // 8, patch_size=3, stride=2, in_chans=embed_dims[1], embed_dim=embed_dims[2])
+
+        self.decoder1 = D_ConvLayer(embed_dims[2], embed_dims[1])  
+        self.decoder2 = D_ConvLayer(embed_dims[1], embed_dims[0])  
+        self.decoder3 = D_ConvLayer(embed_dims[0], embed_dims[0]//4) 
         self.decoder4 = D_ConvLayer(embed_dims[0]//4, embed_dims[0]//8)
         self.decoder5 = D_ConvLayer(embed_dims[0]//8, embed_dims[0]//8)
 
         self.final = nn.Conv2d(embed_dims[0]//8, num_classes, kernel_size=1)
+        self.soft = nn.Softmax(dim=1)
 
     def forward(self, x):
         B = x.shape[0]
-        
-        # Encoder
+        ### Encoder
+        ### Conv Stage
         out = F.relu(F.max_pool2d(self.encoder1(x), 2, 2))
         t1 = out
-        
         out = F.relu(F.max_pool2d(self.encoder2(out), 2, 2))
         t2 = out
-        
         out = F.relu(F.max_pool2d(self.encoder3(out), 2, 2))
         t3 = out
 
-        # Bottleneck
+        ### Tokenized KAN Stage
         out, H, W = self.patch_embed3(out)
-        for blk in self.block1:
+        for i, blk in enumerate(self.block1):
             out = blk(out, H, W)
         out = self.norm3(out)
         out = out.reshape(B, H, W, -1).permute(0, 3, 1, 2).contiguous()
         t4 = out
 
+        ### Bottleneck
         out, H, W = self.patch_embed4(out)
-        for blk in self.block2:
+        for i, blk in enumerate(self.block2):
             out = blk(out, H, W)
         out = self.norm4(out)
         out = out.reshape(B, H, W, -1).permute(0, 3, 1, 2).contiguous()
 
-        # Decoder
-        out = F.relu(F.interpolate(self.decoder1(out), scale_factor=(2,2), mode='bilinear'))
+        ### Decoder
+        out = F.relu(F.interpolate(self.decoder1(out), scale_factor=(2, 2), mode='bilinear'))
         out = torch.add(out, t4)
         _, _, H, W = out.shape
         out = out.flatten(2).transpose(1, 2)
-        for blk in self.dblock1:
+        for i, blk in enumerate(self.dblock1):
             out = blk(out, H, W)
 
         out = self.dnorm3(out)
         out = out.reshape(B, H, W, -1).permute(0, 3, 1, 2).contiguous()
-        out = F.relu(F.interpolate(self.decoder2(out), scale_factor=(2,2), mode='bilinear'))
+        out = F.relu(F.interpolate(self.decoder2(out), scale_factor=(2, 2), mode='bilinear'))
         out = torch.add(out, t3)
         _, _, H, W = out.shape
         out = out.flatten(2).transpose(1, 2)
         
-        for blk in self.dblock2:
+        for i, blk in enumerate(self.dblock2):
             out = blk(out, H, W)
 
         out = self.dnorm4(out)
         out = out.reshape(B, H, W, -1).permute(0, 3, 1, 2).contiguous()
-
-        out = F.relu(F.interpolate(self.decoder3(out), scale_factor=(2,2), mode='bilinear'))
+        out = F.relu(F.interpolate(self.decoder3(out), scale_factor=(2, 2), mode='bilinear'))
         out = torch.add(out, t2)
-        out = F.relu(F.interpolate(self.decoder4(out), scale_factor=(2,2), mode='bilinear'))
+        out = F.relu(F.interpolate(self.decoder4(out), scale_factor=(2, 2), mode='bilinear'))
         out = torch.add(out, t1)
-        out = F.relu(F.interpolate(self.decoder5(out), scale_factor=(2,2), mode='bilinear'))
+        out = F.relu(F.interpolate(self.decoder5(out), scale_factor=(2, 2), mode='bilinear'))
 
         return self.final(out)
