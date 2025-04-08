d22954d621cf48157261b7a636dd9a311269a175
md-ashikulislam <62753529+md-ashikulislam@users.noreply.github.com>
2025-04-08 18:52:14 +0600

    Update

--- Seg_Unet_All/archs.py
+++ Seg_Unet_All/archs.py
@@ -47,7 +47,7 @@ class up_conv(nn.Module):
     def __init__(self,ch_in,ch_out):
         super(up_conv,self).__init__()
         self.up = nn.Sequential(
-            nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),
+            nn.Upsample(scale_factor=2),
             nn.Conv2d(ch_in,ch_out,kernel_size=3,stride=1,padding=1,bias=True),
 		    nn.BatchNorm2d(ch_out),
 			nn.ReLU(inplace=True)
