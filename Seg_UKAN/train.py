1c46a995aabe96701a6e7c4e1c10fd074b98a5a7
md-ashikulislam <62753529+md-ashikulislam@users.noreply.github.com>
2025-04-08 12:19:33 +0600

    Update

--- Seg_UKAN/train.py
+++ Seg_UKAN/train.py
@@ -480,10 +480,10 @@ def main():
         raise NotImplementedError
     
     # # Load the checkpoint
-    checkpoint = torch.load('/kaggle/input/checkpoint10/model.pth')
+    # checkpoint = torch.load('/kaggle/input/checkpoint10/model.pth')
 
-    model.load_state_dict(checkpoint['state_dict'])
-    optimizer.load_state_dict(checkpoint['optimizer'])
+    # model.load_state_dict(checkpoint['state_dict'])
+    # optimizer.load_state_dict(checkpoint['optimizer'])
 
 
     dataset_name = config['dataset']
